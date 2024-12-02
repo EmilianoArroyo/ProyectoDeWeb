e8b293ab6d4f026830c7ea0aec03c62e7e8ba861
Alicia Gomez <100732196+alisogg@users.noreply.github.com>
2024-12-02 15:09:16 -0600

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'error',
+  },
+};
